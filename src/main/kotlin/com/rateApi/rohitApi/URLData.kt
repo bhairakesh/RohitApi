1464489a29e01a574defba472758afcace0ad4ea
lilha <lilhare52@gmail.com>
2022-12-01 14:02:32 +0530

    uprdgf

--- src/main/kotlin/com/rateApi/rohitApi/URLData.kt
+++ src/main/kotlin/com/rateApi/rohitApi/URLData.kt
@@ -1,6 +1,5 @@
 package com.rateApi.rohitApi
 
 object URLData {
-
-    const val rohitApi = "http://3.108.32.126:8080/api/"
+    const val rohitApi = "http://172.105.35.224:3000/"
 }
--- src/main/kotlin/com/rateApi/rohitApi/service/ApiService.kt
+++ src/main/kotlin/com/rateApi/rohitApi/service/ApiService.kt
@@ -3,14 +3,13 @@ package com.rateApi.rohitApi.service
 import com.rateApi.rohitApi.jsonData.Markets
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("rohit/{id}")
-    fun getFancyData(@Path("id") id: String): Call<Markets>
+    @GET("getbm2")
+    fun getFancyData(@Query("eventId") id: String): Call<Markets>
 
-
-    @GET("rohit/{id}")
-    fun getBookMaker(@Path("id") id: String): Call<Markets>
+    @GET("getbm2")
+    fun getBookMaker(@Query("eventId") id: String): Call<Markets>
 }
